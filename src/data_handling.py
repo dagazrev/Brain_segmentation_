344e26aae5bfe32687a223287337833006ae65fc
dagazrev <dagazrev@gmail.com>
2023-12-14 09:17:32 +0100

    masks and tests

--- src/data_handling.py
+++ src/data_handling.py
@@ -1,4 +1,6 @@
 import os
+import numpy as np
+import nibabel as nib
 
 class data_handling:#remaing from dataloader since seeing the pytorch dataloaderxD
     def __init__(self, data_folder):
@@ -40,3 +42,59 @@ class data_handling:#remaing from dataloader since seeing the pytorch dataloader
             mask_filename = os.path.join(target_folder, f"IBSR_{formatted_number}_mask.nii.gz")
             result.append((image_filename, label_filename, mask_filename))
         return result
+    
+    def compare_histograms_top_three(self):
+        training_data = self.retrieve_data('training', 0)
+        validation_data = self.retrieve_data('validation', 0)
+
+        for v_image, _, _ in validation_data:
+            print(f"Comparing for {v_image}:")
+            v_img_nifti = nib.load(v_image)
+            v_img_data = v_img_nifti.get_fdata()
+            v_hist, _ = np.histogram(v_img_data.flatten(), bins=256, range=[0, 256])
+
+            comparisons = []
+            for t_image, _, _ in training_data:
+                t_img_nifti = nib.load(t_image)
+                t_img_data = t_img_nifti.get_fdata()
+                t_hist, _ = np.histogram(t_img_data.flatten(), bins=256, range=[0, 256])
+
+                # Calculate the correlation coefficient
+                coef = np.corrcoef(v_hist, t_hist)[0, 1]
+                comparisons.append((t_image, coef))
+
+            # Sort based on the coefficient
+            comparisons.sort(key=lambda x: x[1], reverse=True)
+
+            # Print top 3 matches
+            for match in comparisons[:3]:
+                print(f"Training Image: {match[0]}, Coefficient: {match[1]}")
+
+    def compare_histograms(self):
+        training_data = self.retrieve_data('training', 0)
+        validation_data = self.retrieve_data('validation', 0)
+
+        top_pairs = []
+
+        for v_image, _, _ in validation_data:
+            top_similarity = 0
+            top_training_image = None
+
+            v_img_nifti = nib.load(v_image)
+            v_img_data = v_img_nifti.get_fdata()
+            v_hist, _ = np.histogram(v_img_data.flatten(), bins=256, range=[0, 256])
+
+            for t_image, _, _ in training_data:
+                t_img_nifti = nib.load(t_image)
+                t_img_data = t_img_nifti.get_fdata()
+                t_hist, _ = np.histogram(t_img_data.flatten(), bins=256, range=[0, 256])
+
+                coef = np.corrcoef(v_hist, t_hist)[0, 1]
+                if coef > top_similarity:
+                    top_similarity = coef
+                    top_training_image = t_image
+
+            print(f"Validation Image: {v_image}, Top Training Image: {top_training_image}, Similarity Coefficient: {top_similarity}")
+            top_pairs.append((v_image, top_training_image))
+
+        return top_pairs
--- src/testing.py
+++ src/testing.py
@@ -2,3 +2,8 @@ import data_handling as dh
 
 obj = dh.data_handling("images")
 print(obj.retrieve_data('training', [9]))
+
+paths = obj.compare_histograms()
+
+parameter_files = ['Par0033similarity.txt', 'Par0033bspline.txt']
+
