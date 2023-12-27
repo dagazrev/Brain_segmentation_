c73d270e9b1e5bf0f8f3d7c1663e2ab09279f6e4
dagazrev <dagazrev@gmail.com>
2023-12-27 12:29:43 +0100

    registering and using transformation matrix on labels - validation

--- src/initial_utils.py
+++ src/initial_utils.py
@@ -1,6 +1,8 @@
 import SimpleITK as sitk
 import itk
 import os
+import nibabel as nib
+import numpy as np
 
 def register_image(fixed_image_path, moving_image_path, fixed_mask_path, output_folder, parameter_files):
     fixed_image = itk.imread(fixed_image_path, itk.F)
@@ -33,11 +35,11 @@ def apply_transformation(moving_label_path, output_folder, transform_param_files
     transformix_transform.SetMovingImage(moving_label)
     transformix_transform.SetOutputDirectory(output_folder)
     transformix_transform.SetTransformParameterMap(sitk.ReadParameterFile(transform_param_files[0]))
-    transformix_transform.SetTransformParameter("FinalBSplineInterpolationOrder", 0)
+    transformix_transform.SetTransformParameter("FinalBSplineInterpolationOrder",'0')
 
     for param_file in transform_param_files[1:]:
         transformix_transform.AddTransformParameterMap(sitk.ReadParameterFile(param_file))
-        transformix_transform.SetTransformParameter("FinalBSplineInterpolationOrder", 0)
+        transformix_transform.SetTransformParameter("FinalBSplineInterpolationOrder", '0')
 
     transformix_transform.Execute()
 
@@ -66,13 +68,11 @@ def register_and_propagate_labels(data_handler, parameter_files):
     for t_image_path, t_label_path, _ in training_data:
         t_folder_name = os.path.basename(os.path.dirname(t_image_path))
         moving_image_path = os.path.join(os.path.dirname(t_image_path), f"{t_folder_name}_PREPR_BF_NL_EQ.nii.gz")
-
-        fixed_image_path = None
-        fixed_mask_path = None
         
         # Find corresponding validation image and mask
         for v_image_path, _, v_mask_path in validation_data:
-            fixed_image_path = v_image_path
+            v_folder_name = os.path.basename(os.path.dirname(v_image_path))
+            fixed_image_path = os.path.join(os.path.dirname(v_image_path), f"{v_folder_name}_PREPR_BF_NL_EQ.nii.gz")
             fixed_mask_path = v_mask_path
 
             # Perform registration
@@ -83,3 +83,56 @@ def register_and_propagate_labels(data_handler, parameter_files):
             moving_label_path = t_label_path
             transform_param_files = find_transform_parameter_files(output_folder)
             apply_transformation(moving_label_path, output_folder, transform_param_files)
+
+def dice_score_per_tissue(true_labels, pred_labels, tissue_value):
+    true_labels = np.squeeze(true_labels)  # Remove singleton dimensions if any
+    pred_labels = np.squeeze(pred_labels)  # Remove singleton dimensions if any
+
+    true_labels_tissue = (true_labels == tissue_value).astype(int)
+    pred_labels_tissue = (pred_labels == tissue_value).astype(int)
+    intersection = np.sum(true_labels_tissue * pred_labels_tissue)
+
+    total = np.sum(true_labels_tissue) + np.sum(pred_labels_tissue)
+    return 2. * intersection / total if total > 0 else 0
+
+
+import csv
+
+def calculate_segmentation_scores(data_handler):
+    """
+    Calculate Dice Score Coefficients for transformed segmentation images against original validation segmentations,
+    and save the results in a CSV file in each validation subfolder.
+    """
+    validation_data = data_handler.retrieve_data('validation', 0)
+
+    for v_image_path, v_label_path, _ in validation_data:
+        v_folder_name = os.path.basename(os.path.dirname(v_image_path))
+        original_seg_path = os.path.join(os.path.dirname(v_label_path), f"{v_folder_name}_seg.nii.gz")
+        original_seg = nib.load(original_seg_path).get_fdata()
+
+        transformed_segs = [file for file in os.listdir(os.path.dirname(v_label_path)) if '_seg_transformed.nii.gz' in file]
+
+        # Prepare CSV file
+        csv_file_path = os.path.join(os.path.dirname(v_label_path), f"{v_folder_name}_dice_scores.csv")
+        with open(csv_file_path, mode='w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',')
+            # Write header
+            csv_writer.writerow(['Transformed Segmentation', 'CSF Dice Score', 'WM Dice Score', 'GM Dice Score'])
+
+            for transformed_seg_file in transformed_segs:
+                transformed_seg_path = os.path.join(os.path.dirname(v_label_path), transformed_seg_file)
+                transformed_seg = nib.load(transformed_seg_path).get_fdata()
+
+                dice_scores = {}
+                for tissue_label in [1, 2, 3]:  # 1: CSF, 2: WM, 3: GM
+                    dice_scores[tissue_label] = dice_score_per_tissue(original_seg, transformed_seg, tissue_label)
+
+                # Write data to CSV
+                csv_writer.writerow([
+                    transformed_seg_file, 
+                    dice_scores[1], 
+                    dice_scores[2], 
+                    dice_scores[3]
+                ])
+
+        print(f"Dice scores for {v_folder_name} saved to {csv_file_path}")
---
+++ src/testing.py
@@ -0,0 +1,11 @@
+import data_handling as dh
+import initial_utils as iu
+obj = dh.data_handling("images")
+
+#obj.process_images(['BF', 'NL', 'EQ'])
+
+parameter_files = ['src\Par0033similarity.txt', 'src\Par0033bspline.txt']
+
+#iu.register_and_propagate_labels(obj, parameter_files)
+
+iu.calculate_segmentation_scores(obj)
